6029b020c4b2892ca00d112fd37442f8a3d4689d
Emerson Frasure <emersonfrasure@gmail.com>
2024-05-06 15:21:41 -0400

    add: Logos, basic structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import logo from "../assets/AirbnbLogo.png"
+
+export default function Navbar() {
+    return (
+        <div>
+            <img src = {logo} alt = "Airbnb Logo"/>
+        </div>
+    )
+}
